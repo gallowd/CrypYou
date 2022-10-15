885e5c9328eb5ebdf7ea4d6b24abb1d75cd49e40
Braulio Zamora <brauliozre@gmail.com>
2022-10-14 22:14:48 -0300

    Navbar complete

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./navbar.css"
+import main_video from "./videos/main_video.mp4"
 
 function App() {
   return (
-    <div className="navbar">
-
+    <div className="main">
+      <video src={main_video} autoPlay loop muted></video>
     </div>
   );
 }

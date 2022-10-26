6f706151d16c2bd3a234c49538f6d6e806bb21a9
Braulio Zamora <brauliozre@gmail.com>
2022-10-26 14:22:49 -0300

    Crypto header video

--- src/screens/Crypto/Crypto.js
+++ src/screens/Crypto/Crypto.js
@@ -1,10 +1,14 @@
 import Navbar from "../../components/UI/Navbar/navbar";
 import Footer from "../../components/UI/Footer/footer";
+import CryptoVideo from "../../videos/cryptoVideo.mp4";
+import "../../components/Crypto/crypto.css"
 
 function Crypto() {
     return (
         <div className="crypto">
             <div className="crypto_header">
+                <div className="video_capa"></div>
+                <video src={CryptoVideo} autoPlay loop muted/>
             </div>
             <Navbar sticky="top"/>
             <Footer/>
